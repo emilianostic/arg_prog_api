3a55ea95cca2e4fb908698d1f5b3e8bf88243ac2
emilianostic <emilianostic@gmail.com>
2023-10-27 19:41:05 -0300

    Agrego fetch de datos reales de API colectivos

---
+++ src/transport/FetchDataTraffic.js
@@ -0,0 +1,14 @@
+const FetchDataTraffic = () => {
+  return fetch(
+    "https://datosabiertos-transporte-apis.buenosaires.gob.ar:443/colectivos/vehiclePositionsSimple?agency_id=63&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6"
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((ex) => {
+      console.error(ex);
+    });
+};
+
+export default FetchDataTraffic;
