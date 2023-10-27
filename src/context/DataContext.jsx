9d5afe3709165779449559cbadceb222aa5a21fc
emilianostic <emilianostic@gmail.com>
2023-10-27 19:43:05 -0300

    Limpieza y formateo de código

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -5,28 +5,28 @@ const DataContext = createContext();
 
 export function DataContextProvider({ children }) {
   const [uvIndexMax, setUvIndexMax] = useState(null);
-  const[loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [sunrise, setSunrise] = useState(null);
   const [sunset, setSunset] = useState(null);
   const [windStatus, setWindStatus] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [hourlyVisibility, setHourlyVisibility] = useState(null);
-  const [rainProbability, setRainProbability] = useState(null)
-const [weatherCode, setWeatherCode] = useState(null)
-const [weatherCode2, setWeatherCode2] = useState(null)
-const [minTemperature_2m, setMinTemperature_2m] = useState(null)
-const [maxTemperature_2m, setMaxTemperature_2m] = useState(null)
-const [dateFormat, setDateFormat] = useState(null)
-const [actualTemp, setActualTemp] = useState(null)
-const [dailyTemp, setDailyTemp] = useState(null)
-const [hourlyTime, setHourlyTime] = useState(null)
+  const [rainProbability, setRainProbability] = useState(null);
+  const [weatherCode, setWeatherCode] = useState(null);
+  const [weatherCode2, setWeatherCode2] = useState(null);
+  const [minTemperature_2m, setMinTemperature_2m] = useState(null);
+  const [maxTemperature_2m, setMaxTemperature_2m] = useState(null);
+  const [dateFormat, setDateFormat] = useState(null);
+  const [actualTemp, setActualTemp] = useState(null);
+  const [dailyTemp, setDailyTemp] = useState(null);
+  const [hourlyTime, setHourlyTime] = useState(null);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     const fetchDataApi = async () => {
       try {
         const data = await FetchData();
-        setLoading(false)
+        setLoading(false);
         setUvIndexMax(data.daily.uv_index_max);
         setSunrise(data.daily.sunrise);
         setSunset(data.daily.sunset);
@@ -34,36 +34,28 @@ const [hourlyTime, setHourlyTime] = useState(null)
         setHumidity(data.hourly.relativehumidity_2m);
         setHourlyVisibility(data.hourly.visibility);
         setRainProbability(data.hourly.precipitation_probability);
-      
-       setMinTemperature_2m(data.hourly.temperature_2m)
-       setMaxTemperature_2m(data.hourly.temperature_2m)
-       setActualTemp(data.current_weather.temperature)
-       setDailyTemp(data.hourly.temperature_2m)
-       setHourlyTime(data.hourly.time)
-       setDateFormat(data.current_weather.time)
-       setWeatherCode(data.current_weather.weathercode)
-       setWeatherCode2(data.current_weather.weathercode)
 
-       console.log(data.current_weather.weathercode)
+        setMinTemperature_2m(data.hourly.temperature_2m);
+        setMaxTemperature_2m(data.hourly.temperature_2m);
+        setActualTemp(data.current_weather.temperature);
+        setDailyTemp(data.hourly.temperature_2m);
+        setHourlyTime(data.hourly.time);
+        setDateFormat(data.current_weather.time);
+        setWeatherCode(data.current_weather.weathercode);
+        setWeatherCode2(data.current_weather.weathercode);
+
+        console.log(data.current_weather.weathercode);
       } catch (error) {
         console.log("Error:", error);
-        
       }
     };
 
     fetchDataApi();
   }, []);
 
-
   if (loading) {
-    
-    return(
-      <div>
-        Cargando...
-      </div>
-    );
+    return <div>Cargando...</div>;
   }
-
 
   return (
     <DataContext.Provider
@@ -75,14 +67,14 @@ const [hourlyTime, setHourlyTime] = useState(null)
         humidity,
         hourlyVisibility,
         rainProbability,
-       minTemperature_2m,
-       maxTemperature_2m,
-       actualTemp,
-       dailyTemp,
-       hourlyTime,
-       dateFormat,
-       weatherCode,
-       weatherCode2,
+        minTemperature_2m,
+        maxTemperature_2m,
+        actualTemp,
+        dailyTemp,
+        hourlyTime,
+        dateFormat,
+        weatherCode,
+        weatherCode2,
         loading,
       }}
     >
