18f15eced56f2adfea23cb8566bac3c8122394ef
emilianostic <emilianostic@gmail.com>
2023-11-16 16:29:45 -0300

    cambio latitud y longitud de DataFetch

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -2,7 +2,7 @@
 
 const FetchData = () => { 
     
-    return fetch('https://api.open-meteo.com/v1/forecast?latitude=-31.7327&longitude=-60.529&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,weathercode,visibility&daily=sunrise,sunset,uv_index_max,uv_index_clear_sky_max&current_weather=true&timezone=America%2FSao_Paulo&forecast_days=1')
+    return fetch('https://api.open-meteo.com/v1/forecast?latitude=-38.95161&longitude=-68.0591&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,weathercode,visibility&daily=sunrise,sunset,uv_index_max,uv_index_clear_sky_max&current_weather=true&timezone=America%2FSao_Paulo&forecast_days=1')
     .then(res => res.json())
     .then(data => {
       
