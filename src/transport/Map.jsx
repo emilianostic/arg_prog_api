36081f86fa0e45b4e99aac399c964e3f19f20293
emilianostic <emilianostic@gmail.com>
2024-03-01 16:43:24 -0300

    haciendo el dropdown3

--- src/transport/Map.jsx
+++ src/transport/Map.jsx
@@ -1,25 +1,60 @@
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import { useState, useEffect} from "react";
+//import { useTraffic } from "./TrafficContext";
 
-import { useTraffic } from "./TrafficContext";
+const Map = () => {
+  const [opcionSeleccionada, setOpcionSeleccionada] = useState('');
+  const [datosApi, setDatosApi] = useState([]);
+  const [datosOtraApi, setDatosOtraApi] = useState([]);
+  const [datosTerceraApi, setDatosTerceraApi] = useState([]);
+  // Función para manejar el cambio de opción
+  const handleChange = (e) => {
+    setOpcionSeleccionada(e.target.value);
+  };
+  useEffect(() => {
 
+    if (opcionSeleccionada) {
+      if (setOpcionSeleccionada ('62')) {
+       
+        // Llamada a la primera API
+        fetch('https://datosabiertos-transporte-apis.buenosaires.gob.ar:443/colectivos/vehiclePositionsSimple?agency_id=62&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6')
+          .then(response => response.json())
+          .then(data => setDatosApi(data))
+          .catch(error => console.error('Error al obtener datos de la API 1:', error));
+      } else if (setOpcionSeleccionada ('63')) {
+        // Llamada a la segunda API
+        fetch('https://datosabiertos-transporte-apis.buenosaires.gob.ar:443/colectivos/vehiclePositionsSimple?agency_id=63&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6')
+          .then(response => response.json())
+          .then(data => setDatosOtraApi(data))
+          .catch(error => console.error('Error al obtener datos de la API 2:', error));
+      }
+      else{
+        fetch('https://datosabiertos-transporte-apis.buenosaires.gob.ar:443/colectivos/vehiclePositionsSimple?agency_id=64&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6')
+        .then(response => response.json())
+        .then(data => setDatosTerceraApi(data))
+        .catch(error => console.error('Error al obtener datos de la API 3:', error));
+      }
+    }
+  }, [opcionSeleccionada]);
 
 
 //let filteredPositions = "";
-const Map = () => {
- const { opcionSeleccionada, dataTraffic, /*selectedDestiny,  datosApi, datosOtraApi, datosTerceraApi */  } = useTraffic();
+/* const Map = () => {
+ const { opcionSeleccionada, dataTraffic, /*selectedDestiny,  datosApi, datosOtraApi, datosTerceraApi */ /* } = useTraffic(); */
 
   //console.log(dataTraffic);
  // console.log(selectedDestiny);
- const handlerOnChange = (e) => {
-  opcionSeleccionada(e.target.value);
-};
+
 
 
   return (
     <>
+    <p>{datosApi}</p>
+    <p>{datosOtraApi}</p>
+    <p>{datosTerceraApi}</p>
       <select
         value={opcionSeleccionada}
-        onChange={(e) => handlerOnChange(e.target.value)}
+        onChange={(e) => handleChange(e.target.value)}
       >
         <>
           <option value="62">línea 62 </option>
@@ -45,7 +80,7 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {dataTraffic.map((data, index) => {
+        {opcionSeleccionada.map((data, index) => {
           return (
             <>
               <Marker position={[data.latitude, data.longitude]} key={index}>
