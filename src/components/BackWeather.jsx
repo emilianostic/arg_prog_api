9bf595a3a1dfb42f7785be4f67c96013b2647035
emilianostic <emilianostic@gmail.com>
2024-03-14 16:42:05 -0300

    creo componente CitySelector y lo coloco en BackWeather

--- src/components/BackWeather.jsx
+++ src/components/BackWeather.jsx
@@ -1,49 +1,54 @@
-
 import { useWeather } from "../context/WeatherDataContext";
-import "../styles.css"
+import "../styles.css";
 import TempDay from "../components/TempDay";
 import MeteoCards from "../components/MeteoCards";
 import MaxTempMinTemp from "../components/MaxTempMinTemp";
 import TempSky from "../components/ActualTempSky";
-import CitySelect from "../components/CitySelector"
-
+import CitySelect from "../components/CitySelector";
 
 const BackWeather = () => {
-  const {weatherCode} = useWeather();
- let weatherClass;
- if(weatherCode === 0 || weatherCode === 1 || weatherCode === 2){
-  weatherClass = "clear"
- }
- else if (weatherCode === 3){
-  weatherClass = "overcast"
- }
- else if (weatherCode === 45 || weatherCode === 48){
-  weatherClass = "fog"
-
- }
- else if( weatherCode === 51 || weatherCode === 53 || weatherClass === 55 || weatherCode === 56 || weatherCode === 57 || weatherCode === 61 || weatherCode === 63 || weatherCode === 65|| weatherCode === 66|| weatherCode === 67|| weatherCode === 80|| weatherCode === 81|| weatherCode === 82){
-  weatherClass = "rain"
- }
- else if (weatherCode === 85 || weatherCode === 86){
-  weatherClass = "snow"
- }
- else{
-  weatherClass = "storm"
- }
+  const { weatherCode } = useWeather();
+  let weatherClass;
+  if (weatherCode === 0 || weatherCode === 1 || weatherCode === 2) {
+    weatherClass = "clear";
+  } else if (weatherCode === 3) {
+    weatherClass = "overcast";
+  } else if (weatherCode === 45 || weatherCode === 48) {
+    weatherClass = "fog";
+  } else if (
+    weatherCode === 51 ||
+    weatherCode === 53 ||
+    weatherClass === 55 ||
+    weatherCode === 56 ||
+    weatherCode === 57 ||
+    weatherCode === 61 ||
+    weatherCode === 63 ||
+    weatherCode === 65 ||
+    weatherCode === 66 ||
+    weatherCode === 67 ||
+    weatherCode === 80 ||
+    weatherCode === 81 ||
+    weatherCode === 82
+  ) {
+    weatherClass = "rain";
+  } else if (weatherCode === 85 || weatherCode === 86) {
+    weatherClass = "snow";
+  } else {
+    weatherClass = "storm";
+  }
 
   return (
     <div className={weatherClass}>
-   <CitySelect/>
-    <div className="temps">
-      
-      <TempSky />
-      <TempDay />
+      <CitySelect />
+      <div className="temps">
+        <TempSky />
+        <TempDay />
+      </div>
+      <div className="meteorology">
+        <MaxTempMinTemp />
+        <MeteoCards />
+      </div>
     </div>
-    <div className="meteorology">
-      <MaxTempMinTemp />
-      <MeteoCards />
-    </div>
-  </div>
   );
 };
 
